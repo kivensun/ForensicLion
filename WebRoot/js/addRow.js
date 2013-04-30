615dad539fdc4a7019efa078a9712adfcfd8998a
kivensun <kivensun@gmail.com>
2013-04-30 20:50:59 +0800

    add addtbtestingrow

--- WebRoot/js/addRow.js
+++ WebRoot/js/addRow.js
@@ -10,6 +10,22 @@ function addRow(tableID) {
 		$(this).find("input").val("");
 	});
 	$("#" + tableID).append(row);
+	$.parser.parse();
+}
+function addtblTestThingRow(){
+	var num = $("#tblTestThing tr").length;
+	var row = $("#tblTestThing  tr").eq(1).clone();
+	row.removeAttr("id");
+	$.each(row.find("td"), function(i) {
+		if (i == 0) {
+			$(this).html(num);
+		}
+		$(this).find("input").val("");
+	});
+	$("#tblTestThing").append(row);
+	$("#tblTestThing>tbody>tr:last").find("td").eq(-3).empty();
+	$("#tblTestThing>tbody>tr:last").find("td").eq(-3).append('<input id="getDate" class="easyui-datebox" required="required" 	name="CaseCheckInfor.getDate"/>');
+	$.parser.parse();
 }
 //table删除最后一行
 function delRow(tableID) {
